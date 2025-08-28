bcfeb51c0a3ba35bd005df1840187d658219db1b
Vishant <vishantmehta0@gmail.com>
2025-08-28 18:02:53 +0530

    minor fix

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createProductReview, } = require('../controllers/productController.js');
+const {
+    getProducts,
+    getProductById,
+    createProduct,
+    updateProduct,
+    deleteProduct,
+    createProductReview
+} = require('../controllers/productController.js');
 const { protect, admin } = require('../middleware/authMiddleware.js');
 
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductById);
-
-//admin only routes
-router.route('/').post(protect, admin, createProduct);
-
+router.route('/')
+    .get(getProducts)
+    .post(protect, admin, createProduct);
 
 router.route('/:id')
+    .get(getProductById)
     .put(protect, admin, updateProduct)
     .delete(protect, admin, deleteProduct);
 
-//review route 
 router.route('/:id/reviews').post(protect, createProductReview);
 
 module.exports = router;
